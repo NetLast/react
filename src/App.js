a7a31282e8a4688a443ff79e39f10cbcdddb263e
Svitlana <ssikora@medallia.com>
2023-11-26 18:24:47 +0200

    initial project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+    <header className="header">
+      <h1>Todo List</h1>
+    </header>
+
+    <main className="todo">
+      <form className="todo__container" id="form">
+        <input type="text" className="todo__input" placeholder="Describe todo item"/>
+        <button type="submit" className="todo__add">+</button>
+      </form>
+
+      <button className="todo__fetch" id="fetch">Fetch All</button>
+
+      <ul className="todo__list" id="list">
+        <li className="todo__item">
+          <input type="checkbox" className="todo__checkbox"/>
+          <p>Task number one</p>
+          <button className="todo__remove remove">x</button>
+        </li>
+        <li className="todo__item">
+          <input type="checkbox" className="todo__checkbox"/>
+          <p>Task number two</p>
+          <button className="todo__remove remove">x</button>
+        </li>
+        <li className="todo__item">
+          <input type="checkbox" className="todo__checkbox"/>
+          <p>Task number three</p>
+          <button className="todo__remove remove">x</button>
+        </li>
+      </ul>
+    </main>
+  </div>
   );
 }
 
